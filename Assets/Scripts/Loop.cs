03d186498b7818726b191e4e94d84afa13873f08
Skulsta <sti.sku@gmail.com>
2018-01-27 18:34:10 +0100

    Lagt til avatar til enemy

---
+++ Assets/Scripts/Loop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour {
+
+    // Use this for initialization
+    Animator anim = new Animator();
+
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.StartPlayback();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
