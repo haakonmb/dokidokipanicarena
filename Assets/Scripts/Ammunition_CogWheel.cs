7fea2fc83b5b7035d9010c043d391fab81cf7aea
sindrekjr <sindre@sindrekjr.no>
2018-01-27 11:31:47 +0100

    Destroy() for Ammunition_CogWheel. Lagt til bruk av fremtidig metode damage() hos Enemy.

--- Assets/Scripts/Ammunition_CogWheel.cs
+++ Assets/Scripts/Ammunition_CogWheel.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Ammunition_CogWheel : MonoBehaviour {
 
     private Rigidbody rb;
+    private float countdown = 5;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0.0) Destroy(gameObject);
 	}
 
     void OnCollisionEnter(Collision collision) {
         Debug.Log("You've hit the " + collision.gameObject.name.ToLower() + ". Well done.");
+        //if (rb.transform.parent.tag == "Player" && collision.gameObject.tag == "Enemy") collision.gameObject.damage(5);
+
+        //Destroy(gameObject);
     }
 }
