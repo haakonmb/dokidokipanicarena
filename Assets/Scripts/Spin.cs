7abc2b44e20c03e7a7264ef731873780c6452014
Patrick Solbue <patricksolbue@gmail.com>
2018-01-27 21:39:00 +0100

    Changed stuff and added ScoreManager

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class Spin : MonoBehaviour
 {
     public float speed = 10f;
-
+    public Vector3 RotateAmount;
 
     void Update()
     {
         transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        transform.Rotate(RotateAmount * Time.deltaTime);
     }
 }
