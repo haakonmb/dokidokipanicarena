183021fb7246cb6aa26c40022deb8ede3794b2b4
Skulsta <sti.sku@gmail.com>
2018-01-27 22:44:30 +0100

    they attack

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public Rigidbody rb;
+    float timer = 0f;
+    public float frequency = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer >= frequency)
+        {
+            Rigidbody cw = Instantiate(rb, transform.position, Quaternion.identity);
+            timer = 0f;
+        }
+    }
+}
