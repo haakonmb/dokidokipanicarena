5e1f6426a8fecccfe097f34b2d3972d0c6ea90a5
Patrick Solbue <patricksolbue@gmail.com>
2018-01-27 11:50:11 +0100

    Added placeholder arena and ficed nav mesh.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody cw = Instantiate(rb, transform.position, Quaternion.identity);
+    }
+
+    Vector3 spawnLoc()
+    {
+        Vector3 loc = new Vector3(0, 0, 0);
+
+
+        return loc;
+    }
+}
